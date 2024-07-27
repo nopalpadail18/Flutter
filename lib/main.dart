f1033e17f9014ecf1f4c25d6c2bc0316f7357ec2
nopalpadail18 <nopalpadail@gmail.com>
2024-07-27 21:45:47 +0800

    Feat : Persiapan

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MyApp());
+  runApp( const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Kendari Coding - Course'),
-        ),
-        body: Center(child: Text('Hello World'),),
-      ),
-    );
-  }
-}
+// void main() => runApp(MyColumn());
+
